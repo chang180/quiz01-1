7d66537f3e3ef1e35d9e9d3bc08ba532b7e21eb3
張 建文 <chang180@gmail.com>
2020-11-05 13:10:42 +0800

    update to day-02

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>校園科技大學校園資訊系統</title>
+    
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.css' />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="container">
+<div class="header w-100">
+    <img src="{{asset('img/01B01.jpg')}} " alt="" class="w-100">
+    </div>
+<div class="main d-flex" style="height:568px;">
+    @yield("main")
+</div>
+<div class="footer w-100">
+    <div class="bg-yellow-300 text-center" style="line-height:100px">頁尾版權</div>
+</div>
+
+
+</div>
+
+
+    
+    
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
+</body>
+</html>
+@yield("script")
