198f2d81b44aa8de92aa23c7757e656a2245eec0
張 建文 <chang180@gmail.com>
2020-12-24 10:38:25 +0800

    update to vue-day-08

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,88 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+
+
+const app = {
+    data() {
+
+        return {
+            menus: null,
+            images: null,
+            mvims: null,
+            newss: null,
+            site: null,
+            show: false,
+        }
+    },
+    methods: {
+        switchImg(type) {
+            let imgs = this.images.data
+            let page = this.images.page
+            switch (type) {
+                case 'up':
+                    page = (page > 0) ? page - 1 : page
+                    break
+                case 'down':
+                    page = (page < imgs.length - 3) ? page + 1 : page
+                    break
+            }
+            imgs.map((img, idx) => {
+                if (idx >= page && idx <= page + 2) {
+                    img.show = true
+                } else {
+                    img.show = false
+                }
+                return img
+            })
+            this.images.data = imgs
+            this.images.page = page
+        }
+    },
+    mounted() {
+        axios.get("/api")
+            .then((res) => {
+                // console.log(res.data)
+                this.site = res.data.site
+                this.menus = res.data.menus
+                this.images = res.data.images
+                this.newss = res.data.news
+                this.mvims = res.data.mvims
+                this.show = true
+            })
+        let m = 1
+        setInterval(() => {
+            this.mvims.map((mv, idx) => {
+                mv.show = (idx == m) ? true : false
+                return mv
+            })
+            m = (m + 1) % this.mvims.length
+        }, 3000);
+    }
+    // mounted(){
+    //     this.switchImg('up')
+    // }
+}
+
+createApp(app).component('marquee', {
+    // props: ['text'],
+    template: `
+                                <div class="relative w-100 h-8 overflow-hidden" ref="marquee">
+                                <div class="absolute w-max" ref="content">
+                                <slot></slot>
+                                </div>
+                                </div>            
+                                `,
+    mounted() {
+        let marquee = this.$refs.marquee.offsetWidth
+        let content = this.$refs.content.offsetWidth
+            // console.log(marquee, content)
+        this.$refs.content.style.right = (0 - content) + "px"
+        let pos = 0 - content
+        setInterval(() => {
+            pos++
+            this.$refs.content.style.right = pos + "px"
+            if (pos >= marquee) pos = 0 - content
+        }, 15);
+    }
+}).mount('#app')
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,12 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>校園科技大學校園資訊系統</title>
-    <script src="https://unpkg.com/vue@next"></script>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.css'/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js'></script>
 </head>
 
 <body>
@@ -37,6 +35,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
         integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
     </script>
+    <script src="./js/app.js"></script>
 </body>
 
 </html>
